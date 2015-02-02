f3b64d9882c67eed607004fad1c5e3babd5de69c
Rasheed Abdul-Aziz <squeedee@gmail.com>
2015-02-02 09:24:49 -0500

    Downcase xml attribute names
    [#87386754]

--- structure101/datafiles/datafile.go
+++ structure101/datafiles/datafile.go
@@ -6,13 +6,13 @@ import (
 
 type Module struct {
 	XMLName xml.Name `xml:"module"`
-	Name    string   `xml:",attr"`
-	Id      int      `xml:",attr"`
-	Type    string   `xml:",attr"`
+	Name    string   `xml:"name,attr"`
+	Id      int      `xml:"id,attr"`
+	Type    string   `xml:"type,attr"`
 }
 
 type DataFile struct {
 	XMLName xml.Name `xml:"data"`
-	Flavor  string   `xml:",attr"`
+	Flavor  string   `xml:"flavor,attr"`
 	Modules []Module
 }
