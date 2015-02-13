a892606644596da02d9eba5f1ae73d1b2e1e46a7
Rasheed Abdul-Aziz <squeedee@gmail.com>
2015-02-12 21:53:38 -0500

    Fix issue when GOPATH is unset

--- go-flavor-parser/go-flavor-parser.go
+++ go-flavor-parser/go-flavor-parser.go
@@ -3,10 +3,12 @@ package main
 import (
 	. "github.com/bunniesandbeatings/go-flavor-parser/parser"
 
-	"github.com/kisielk/gotool"
+	"github.com/bunniesandbeatings/gotool"
 
 	"flag"
 	"fmt"
+	"github.com/tonnerre/golang-pretty"
+
 	"os"
 	"go/build"
 	"io/ioutil"
@@ -14,7 +16,9 @@ import (
 
 var (
 	goPath string
+	goRoot string
 	outputPath string
+	importSpec string
 )
 
 func usage() {
@@ -26,36 +30,49 @@ func usage() {
 }
 
 func main() {
-	projectImportPaths := importPathsFromCommandLine()
+	handleCommandLine()
+
+	buildContext := buildContext()
+	
+	gotool.SetContext(buildContext)
+	projectImportPaths := gotool.ImportPaths([]string{importSpec})
 
 	parser := NewParser()
 
-	if goPath != "" {
-		buildContext := build.Default
-		buildContext.GOPATH = goPath
-
-		parser.Packages.BuildContext = buildContext
-	}
+	fmt.Printf("\n*** Context ***\n%# v\n\n", pretty.Formatter(buildContext))
+	parser.Packages.BuildContext = buildContext
 
 	fmt.Printf("Using GOPATH='%s'\n", parser.Packages.BuildContext.GOPATH)
-	
+
 	parser.AddImportPaths(projectImportPaths)
 	fmt.Printf("Ran with arguments: %s\n", os.Args)
 
 	ioutil.WriteFile(outputPath, parser.DataFileXML(), 0644)
-	
+
 	fmt.Printf("Output written to '%s'\n", outputPath)
 }
 
-func importPathsFromCommandLine() []string {
+func handleCommandLine() {
 	flag.StringVar(&goPath, "gopath", os.Getenv("GOPATH"), "allows you to choose a different GOPATH to use during analysis")
+	flag.StringVar(&goRoot, "goroot", os.Getenv("GOROOT"), "allows you to choose a different GOROOT to use during analysis")
 	flag.StringVar(&outputPath, "output", "./output.xml", "where to output the result of the analysis")
 
 	flag.Usage = usage
 	flag.Parse()
 
-	importSpec := []string{flag.Arg(0)}
-
-	return gotool.ImportPaths(importSpec)
+	importSpec = flag.Arg(0)
 }
 
+func buildContext() build.Context {
+	buildContext := build.Default
+
+	if goPath != "" {
+		buildContext.GOPATH = goPath
+	}
+
+	if goRoot != "" {
+		buildContext.GOROOT = goRoot
+	}
+
+	return buildContext
+}
