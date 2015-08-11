b45e189d3664f848f7b7bdc6b2bb420b4ffbbb02
Brandon Shroyer and Rasheed Abdul-Aziz <hakday-pair+brandon+rasheed@pivotal.io>
2015-08-11 16:25:55 -0400

    Now has files and structs and interfaces

--- go-flavor-parser/go-flavor-parser.go
+++ go-flavor-parser/go-flavor-parser.go
@@ -10,22 +10,101 @@ import (
 	"os"
 	"runtime/debug"
 
+	"strings"
+
+	"go/parser"
+	"go/token"
+
 	"github.com/bunniesandbeatings/gotool"
 	"github.com/davecgh/go-spew/spew"
-	"strings"
+	. "go/ast"
+	"reflect"
 )
+
+type AFakeInterface interface {
+	Foo() string
+}
+
+type BFakeInterface interface {
+	AFakeInterface
+	Bar() string
+}
+
+type FileNode struct {
+	PublicStructs []string
+	PublicInterfaces []string
+	PublicFuncs []string
+}
+
 
 func NewPathNode() (pathNode *PathNode) {
 	pathNode = &PathNode{
-		Children: make(map[string]*PathNode),
+		PathChildren: make(map[string]*PathNode),
 	}
 
 	return
 }
 
+
 type PathNode struct {
-	Children map[string]*PathNode
-	Package  string
+	PathChildren map[string]*PathNode
+	FileChildren map[string]*FileNode
+}
+
+// VISITORS
+
+type DumpVisitor struct {
+}
+
+func (visitor DumpVisitor) Visit(node Node) Visitor {
+	fmt.Println(reflect.TypeOf(node))
+	return visitor
+}
+
+
+type TypeSpecVisitor struct {
+	File *FileNode
+	TypeSpec *TypeSpec
+}
+
+
+func (visitor TypeSpecVisitor) Visit(node Node) Visitor {
+	switch node.(type) {
+	case *InterfaceType:
+		visitor.File.PublicInterfaces = append(visitor.File.PublicInterfaces, visitor.TypeSpec.Name.Name)
+	case *StructType:
+		visitor.File.PublicStructs = append(visitor.File.PublicStructs, visitor.TypeSpec.Name.Name)
+	}
+  return nil
+}
+
+type SpecVisitor struct {
+	File *FileNode
+}
+
+func (visitor SpecVisitor) Visit(node Node) Visitor {
+	switch t := node.(type) {
+	case *TypeSpec:
+		return TypeSpecVisitor{
+			File: visitor.File,
+			TypeSpec: t,
+		}
+	}
+	return nil
+}
+
+type DeclVisitor struct {
+	File *FileNode
+}
+
+func (visitor DeclVisitor) Visit(node Node) Visitor {
+	switch node.(type) {
+	case *GenDecl:
+		return SpecVisitor{
+			File: visitor.File,
+		}
+	}
+	return visitor
 }
 
 func usage() {
@@ -41,15 +120,6 @@ func importPaths(buildContext build.Context, importSpec string) []string {
 	return gotool.ImportPaths([]string{importSpec})
 }
 
-func mergeFiles(importPath string, files []string) (filesWithPaths []string) {
-	filesWithPaths = []string{}
-	for _, filename := range files {
-		filesWithPaths = append(filesWithPaths, importPath+"/"+filename)
-	}
-
-	return
-}
-
 func updateASTWithPackage(pkg *build.Package, ast *PathNode) {
 
 	path := strings.Split(pkg.ImportPath, "/")
@@ -57,18 +127,38 @@ func updateASTWithPackage(pkg *build.Package, ast *PathNode) {
 	currentNode := ast
 
 	for _, pathSection := range path {
-		if _, found := currentNode.Children[pathSection]; !found {
-			currentNode.Children[pathSection] = NewPathNode()
+		if _, found := currentNode.PathChildren[pathSection]; !found {
+			currentNode.PathChildren[pathSection] = NewPathNode()
 		}
 
-		currentNode = currentNode.Children[pathSection]
+		currentNode = currentNode.PathChildren[pathSection]
 	}
 
-	currentNode.Package = pkg.Name
+	fset := token.NewFileSet()
+
+	currentNode.FileChildren = make(map[string]*FileNode)
+
+	for _, filename := range pkg.GoFiles {
+		// TODO: whats the portable way?
+		filepath := pkg.Dir + "/" + filename
+		astFile, err := parser.ParseFile(fset, filepath, nil, 0)
+
+//		spew.Dump(astFile)
+
+		if err != nil {
+			log.Printf("Error %s when parsing file %s\n", err, filepath)
+			currentNode.FileChildren[filename] = nil
+		} else {
+			currentNode.FileChildren[filename] = &FileNode{}
+
+			Walk(DeclVisitor{ File: currentNode.FileChildren[filename] }, astFile)
+
+		}
+
+	}
 }
 
 func main() {
-
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Fprintf(os.Stderr, "Exception: %v\n", err)
@@ -78,27 +168,22 @@ func main() {
 	}()
 
 	commandContext := contexts.CreateCommandContext(usage)
+
 	buildContext := contexts.CreateBuildContext(commandContext)
 
 	ast := NewPathNode()
 
 	importPaths := importPaths(buildContext, commandContext.ImportSpec)
 
-//	fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> DEBUG: ImportPaths")
-//	spew.Dump(importPaths)
-//	fmt.Println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DEBUG: ImportPaths\n\n")
-
-	//	fset := token.NewFileSet()
 
 	for _, importPath := range importPaths {
 		fmt.Println(importPath)
 
 		buildPackage, _ := buildContext.Import(importPath, ".", 0)
 
-//		spew.Dump(buildPackage)
+		//		spew.Dump(buildPackage)
 
 		updateASTWithPackage(buildPackage, ast)
-
 	}
 
 	fmt.Println(">>>> DEBUG: ast")
