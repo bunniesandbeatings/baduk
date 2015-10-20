48d878810268bb926944788ebb96d43952d3c1c1
Glyn Normington <gnormington@pivotal.io>
2015-10-20 14:05:20 +0100

    Fix basic method set matching algorithm

--- parser/method_set_test.go
+++ parser/method_set_test.go
@@ -12,21 +12,38 @@ import (
 
 func TestMatchSimpleMethodSet(t *testing.T) {
 	pkg := parseMethodSetsPackage(t)
+	checkMethodSetMatchSize(t, pkg, "Mixer", 1)
 	checkMethodSetMatch(t, pkg, "Mixer", "test_method_sets.ConcreteMixer")
 }
 
 func TestMatchMultiImplsMethodSet(t *testing.T) {
 	pkg := parseMethodSetsPackage(t)
+	checkMethodSetMatchSize(t, pkg, "Shape", 2)
 	checkMethodSetMatch(t, pkg, "Shape", "test_method_sets.Circle")
 	checkMethodSetMatch(t, pkg, "Shape", "test_method_sets.Rectangle")
 }
 
 func TestMatchMultiIfacesMethodSet(t *testing.T) {
 	pkg := parseMethodSetsPackage(t)
+	checkMethodSetMatchSize(t, pkg, "Shape1D", 2)
 	checkMethodSetMatch(t, pkg, "Shape1D", "test_method_sets.Circle")
 	checkMethodSetMatch(t, pkg, "Shape1D", "test_method_sets.Rectangle")
-	checkMethodSetMatch(t, pkg, "Shape1D", "test_method_sets.Rectangle")
+
+	checkMethodSetMatchSize(t, pkg, "Shape2D", 2)
+	checkMethodSetMatch(t, pkg, "Shape2D", "test_method_sets.Circle")
 	checkMethodSetMatch(t, pkg, "Shape2D", "test_method_sets.Rectangle")
+}
+
+func checkMethodSetMatchSize(t *testing.T, pkg *architecture.Package, ifaceName string, expectedNumImplementers int) {
+	iface := findInterface(pkg.Interfaces, ifaceName)
+	if iface == nil {
+		t.Errorf("Could not find interface %s in package %s", ifaceName, pkg.Name)
+		t.FailNow()
+	}
+
+	if expectedNumImplementers != len(iface.Implementers) {
+		t.Errorf("Interface %s is implemented by %d types, expected %d types: %#v", ifaceName, len(iface.Implementers), expectedNumImplementers, iface.Implementers)
+	}
 }
 
 func checkMethodSetMatch(t *testing.T, pkg *architecture.Package, ifaceName, concreteTypeName string) {
